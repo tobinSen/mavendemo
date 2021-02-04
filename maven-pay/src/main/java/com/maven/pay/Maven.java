fecae4eec7e11d5c5bb243a25942ae41ad26b80b
tongjian <1245007716@qq.com>
2021-02-04 15:20:16 +0800

    cherry pick

--- maven-pay/src/main/java/com/maven/pay/Maven.java
+++ maven-pay/src/main/java/com/maven/pay/Maven.java
@@ -10,35 +10,10 @@ package com.maven.pay;
  * 2.soft: 回退到某个版本，只回退了commit的信息，不会恢复到index file一级，若要提交重新commit。
  *
  * 3.Hard:彻底回退到某个版本，本地的源码也会变为上一个版本的内容。
- *
- *
- *
- * 1、回滚
- * 2、tag
- * 3、补丁 文件还是面板
- * 4、cherry pick 摘樱桃
- *
- * sss
  */
 public interface Maven {
 
     public static void main(String[] args) {
         System.out.println("cherry pick");
-        System.out.println("second cherry pick");
-        System.out.println("four cherry pick");
-        System.out.println("five cherry pick");
-
-        System.out.println("six cherry pick");
-        System.out.println("seven cherry pick");
-
-
-        System.out.println("nine cherry pick");
-        System.out.println("ten cherry pick");
-
-
-        System.out.println("ele cherry pick");
-        System.out.println("two cherry pick");
-
-
     }
 }
