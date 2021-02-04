76259a33c2f34ac9c12e01a8909909a05a4e2486
tongjian <1245007716@qq.com>
2021-02-04 15:42:28 +0800

    check

--- maven-pay/src/main/java/com/maven/pay/Seven.java
+++ maven-pay/src/main/java/com/maven/pay/Seven.java
@@ -1,7 +1,7 @@
 package com.maven.pay;
 
-public class main {
+public class Seven {
     public static void main(String[] args) {
-        System.out.println("main");
+        System.out.println("112321323213");
     }
 }
